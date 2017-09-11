f1301830b6e1c3bdd1d08a0fc12315da8974e850
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-11 13:25:14 +0200

    IntroduceYourself.java done

---
+++ week-02/day-01/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Dedeo Zsofia");
+        System.out.println(26);
+        System.out.println(1.72);
+    }
+}
