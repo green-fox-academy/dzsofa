69c4e8476b95134086a917becf67b569904c6ae9
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-11 13:21:23 +0200

    HelloOthers done

---
+++ week-02/day-01/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Barbi!");
+        System.out.println("Hello, Agi!");
+        System.out.println("Hello, Viktor!");
+    }
+}
