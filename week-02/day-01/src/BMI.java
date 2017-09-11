c5c3875a61bedb4d146dc93fe1d988369d84753b
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-11 14:10:20 +0200

    Swap task done

---
+++ week-02/day-01/src/BMI.java
@@ -0,0 +1,9 @@
+public class BMI {
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+        // Print the Body mass index (BMI) based on these values
+
+        System.out.println("Your BMI is" + 81.2 / 1.78 );
+    }
+}
---
+++ week-02/day-01/src/Swap.java
@@ -0,0 +1,10 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a = 123;
+        int b = 526;
+        // Swap the values of the variables
+
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
