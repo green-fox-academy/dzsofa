9d58711878874af2c449adddbd77c3f9f3b34122
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-11 16:43:51 +0200

    PrintEven done

---
+++ week-02/day-01/src/PrintEven.java
@@ -0,0 +1,9 @@
+public class PrintEven {
+    // Create a program that prints all the even numbers between 0 and 500
+    public static void main(String[] args) {
+        int e = 0;
+        while (e < 500) {
+            System.out.println(e += 2);
+        }
+    }
+}
