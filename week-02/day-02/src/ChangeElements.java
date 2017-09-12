e3609b43e4ab4580e40822afc13a1b02a4e382fa
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-12 16:24:07 +0200

    w02-d02 Ex10SumElements

---
+++ week-02/day-02/src/ChangeElements.java
@@ -0,0 +1,14 @@
+public class ChangeElements {
+    //# - Create an array variable named `s`
+    //#   with the following content: `[1, 2, 3, 8, 5, 6]`
+    //# - Change the 8 to 4
+    //# - Print the fourth element
+    public static void main(String[] args) {
+        int[] s = {1, 2, 3, 8, 5, 6};
+
+        s[3] = 4;
+
+        System.out.println(s[3]);
+    }
+
+}
