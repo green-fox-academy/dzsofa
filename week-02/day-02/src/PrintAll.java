02f333bcea61d6fcf254e67855e83c5af4c237f0
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-12 16:40:36 +0200

    w02-d02 Ex12 PrintAll

---
+++ week-02/day-02/src/PrintAll.java
@@ -0,0 +1,13 @@
+public class PrintAll {
+    // - Create an array variable named `af`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print all the elements of `af`
+    public static void main(String[] args) {
+        int[] af = {4, 5, 6, 7};
+
+        for (int i = 0; i < af.length; i++)
+
+        System.out.println(af[i]);
+
+    }
+}
