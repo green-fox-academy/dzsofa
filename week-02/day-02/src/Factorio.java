acd74d738fd9e893c9466b647540de2514ecbc61
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-12 15:03:38 +0200

    w02-d02 Ex5 Factorial

--- week-02/day-02/src/Factorio.java
+++ week-02/day-02/src/Factorio.java
@@ -4,11 +4,17 @@ public class Factorio {
 
     // - Create a function called `factorio`
     //   that returns it's input's factorial
+
     public static void main(String[] args) {
+        System.out.println(factorio(4));
 
     }
     public static int factorio(int number) {
-        return 0;
+        int factorial = 1;                    /* has to be bigger than 0, because multiplication with 0 is 0 */
+        for (int i = 1; i <= number; i++){    /* i has to be bigger than 0, see above*/
+            factorial *= i;
+        }
+        return factorial;
 
     }
 
