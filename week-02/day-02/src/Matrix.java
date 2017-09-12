cb5eb3796817f161b479b86e1b0016b3a3242b7a
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-12 20:32:02 +0200

    w02-d02 Ex13 Matrix

--- week-02/day-02/src/Matrix.java
+++ week-02/day-02/src/Matrix.java
@@ -9,7 +9,18 @@ public class Matrix {
     //
     // - Print this two dimensional array to the output
     public static void main(String[] args) {
-       int[][] multiD = {{1}, {1}};
+       int[][] multiD = new int[4][4];
+
+       for (int i = 0; i < 4; i++) {
+           for (int j = 0; j < 4; j++) {
+               if (i == j) {
+                   multiD[i][j] = 1;
+               } else multiD[i][j] = 0;
+               System.out.print(multiD[i][j]); //here we have all the values in one line
+           }
+           System.out.println();  /*with this print, that has to be AFTER the first for, but
+                                    BEFORE the second, we break the line after the fourth element */
+       }
 
 
 
--- week-02/day-02/src/Summing.java
+++ week-02/day-02/src/Summing.java
@@ -1,12 +1,12 @@
 public class Summing {
+    //  Create the usual class wrapper
+    //  and main method on your own.
+
+    // - Write a function called `sum` that sum all the numbers
+    //   until the given parameter
+
     public static void main(String[] args) {
         System.out.println(Sum(9));
-        //  Create the usual class wrapper
-        //  and main method on your own.
-
-        // - Write a function called `sum` that sum all the numbers
-        //   until the given parameter
-
     }
     public static int Sum(int num) {
         int sum = 0;
