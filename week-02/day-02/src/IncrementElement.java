2b8957d77c6c83813f9eab6bbcaec21ee6283f4e
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-12 16:26:46 +0200

    w02-d02 Ex11 IncrementElement

---
+++ week-02/day-02/src/IncrementElement.java
@@ -0,0 +1,13 @@
+public class IncrementElement {
+    //- Create an array variable named `t`
+    //with the following content: `[1, 2, 3, 4, 5]`
+    //- Increment the third element
+    //- Print the third element
+    public static void main(String[] args) {
+        int[] t = {1, 2, 3, 4, 5};
+
+        t[2]++;
+
+        System.out.println(t[2]);
+    }
+}
