e4ef49e95eb59db0b155e6d87beaa947a4866de1
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-13 15:03:19 +0200

    w02-d03 Ex5 Reverse

---
+++ week-02/day-03/src/IsInList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class IsInList {
+    public static void main(String... args){
+        ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
+
+        //if (list.containsAll(Integer: 4, 8, 12, 26)) {
+
+
+
+        // Check if list contains all of the following elements: 4,8,12,16
+        // Print "true" if it contains all, otherwise print "false"
+        // Can you do both the different approaches you tried in the previous one?
+
+    }
+}
--- week-02/day-03/src/Reverse.java
+++ week-02/day-03/src/Reverse.java
@@ -1,22 +1,16 @@
 public class Reverse {
-    public static void main(String... args){
+    public static void main(String... args) {
         String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
 
         // Create a function that can reverse a String, which is passed as the parameter
         // Use it on this reversed string to check it!
         // Try to solve this using charAt() first, and optionally anything else after.
-
-//        StringBuilder reverse = new StringBuilder();
-//        reverse.append(reversed);
-//        reverse.reverse();
-//        for (int i = 0; i < reverse.length(); i++) {
-//            System.out.print(reverse.charAt(i));
-//        }
-
+        System.out.println(reverse(reversed));
     }
+
     public static String reverse(String source) {
         String reverse = "";
-        for (int i = source.length(); i >= 0; i--) {
+        for (int i = source.length() - 1; i >= 0; i--) {
             reverse = reverse + source.charAt(i);
         }
         return reverse;
