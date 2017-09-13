3af31973e37bab8ddd1d9608a528fb01c63549ab
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-13 14:45:28 +0200

    w02-d03 Ex10 ElementFinder 1st solution found

---
+++ week-02/day-03/src/ElementFinder.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ElementFinder {
+    public static void main(String... args){
+        ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
+
+        // Check if the arrayList contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+        // Do this again with a different solution using different list functions!
+        if (arrayList.contains(7)) {
+            System.out.println("Hooooray!");
+        } else {
+            System.out.println("Noooo");
+        }
+
+    }
+}
+
