a6cfd9e6e5f1c14f2e489a6864ebf80b5d1e703e
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-14 15:50:34 +0200

    w02-d04 AppendA with Arrays from Tuesday done

--- week-02/day-04/src/Colors.java
+++ week-02/day-04/src/Colors.java
@@ -13,5 +13,7 @@ public class Colors {
 
     public static void main(String[] args) {
         String[][] colors = {{"lime", "forest green", "olive", "pale green", "spring green"}, {"orange red", "red", "tomato"}, {"orchid", "violet", "pink", "hot pink"}};
+    
     }
+
 }
