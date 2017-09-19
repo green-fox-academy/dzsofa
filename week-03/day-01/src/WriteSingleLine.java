81ee98792c215220c9a2dece60742ca751486e1e
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-19 12:07:13 +0200

    WriteSingleLine

--- week-03/day-01/src/WriteSingleLine.java
+++ week-03/day-01/src/WriteSingleLine.java
@@ -1,15 +1,25 @@
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WriteSingleLine {
-  public static void main(String[] args) {
-    // Open a file called "my-file.txt"
-    // Write your name in it as a single line
-    // If the program is unable to write the file,
-    // then it should print an error message like: "Unable to write file: my-file.txt"
+    public static void main(String[] args) {
+        // Open a file called "my-file.txt"
+        // Write your name in it as a single line
+        // If the program is unable to write the file,
+        // then it should print an error message like: "Unable to write file: my-file.txt"
+        List<String> myLine = new ArrayList<>();
+        myLine.add("Zsofia Dedeo");
+        Path myPath = Paths.get("src/my-text.txt");
 
-
-  }
+        try {
+            Files.write(myPath, myLine);
+        } catch (IOException e) {
+            System.out.println("Unable to write file: " + myPath);
+        }
+    }
 }
 
