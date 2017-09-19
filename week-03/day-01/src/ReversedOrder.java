16326ad62c56f9808d9651f535b8f99bc486e66f
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-19 15:54:52 +0200

    ReversedOrder

--- week-03/day-01/src/ReversedOrder.java
+++ week-03/day-01/src/ReversedOrder.java
@@ -1,8 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class ReversedOrder {
 
     public static void main(String[] args) {
         // Create a method that decrypts reversed-order.txt
+        reverse("src/reversed-order.txt");
 
-        
+    }
+
+    public static void reverse(String myFile) {
+        Path myPath = Paths.get(myFile);
+        try {
+            List<String> textContent = Files.readAllLines(myPath);
+
+            for (int i = textContent.size() - 1; i > 0; i--) {// length - 1!!!!!
+                System.out.print(textContent.get(i));
+                System.out.println(); // not to be forgotten, or everything will be on one line
+            }
+        } catch (IOException e) {
+            System.out.println("Houston, we have a problem");
+        }
+
     }
 }
