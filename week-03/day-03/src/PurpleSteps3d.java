04042376d1391f730fb368357ee50b2fa99407c4
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-20 14:49:08 +0200

    PurpleSteps3d

--- week-03/day-03/src/PurpleSteps3d.java
+++ week-03/day-03/src/PurpleSteps3d.java
@@ -6,15 +6,23 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class PurpleSteps3d {
 
-    public static void mainDraw(Graphics graphics){
-      // reproduce this:
-      // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
+    public static void mainDraw(Graphics graphics) {
+        // reproduce this:
+        // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
+        int[] startingPoints = {10, 10};
 
-
+        for (int i = 0; i < 6; i++) {
+            graphics.setColor(Color.BLACK);
+            graphics.drawRect(startingPoints[0], startingPoints[0], startingPoints[1], startingPoints[1]);
+            graphics.setColor(new Color(187, 0, 236));
+            graphics.fillRect(startingPoints[0], startingPoints[0], startingPoints[1], startingPoints[1]);
+            startingPoints[0] += startingPoints[1];
+            startingPoints[1] += 10;
+        }
 
     }
 
-//    Don't touch the code below
+    //    Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 343;
 
@@ -26,7 +34,8 @@ public class PurpleSteps3d {
         jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
     }
-    static class ImagePanel extends JPanel{
+
+    static class ImagePanel extends JPanel {
         @Override
         protected void paintComponent(Graphics graphics) {
             super.paintComponent(graphics);
