1edf9803b0e48d97edc73368f413905d8f4c85b0
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-21 12:43:25 +0200

    week03/day04 Fibonacci done

--- week-03/day-04/src/Fibonacci.java
+++ week-03/day-04/src/Fibonacci.java
@@ -1,3 +1,17 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(10));
+    }
+
+    public static int fibonacci(int myFib) {
+        if (myFib == 0) {
+            return 0;
+        } else if (myFib == 1) {
+            return 1;
+        }
+        return fibonacci(myFib - 1) + fibonacci(myFib - 2);
+    }
+}
 // The fibonacci sequence is a famous bit of mathematics, and it happens to
 // have a recursive definition. The first two values in the sequence are
 // 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the
--- week-03/day-04/src/String1.java
+++ week-03/day-04/src/String1.java
@@ -1,2 +1,16 @@
+public class String1 {
+    public static void main(String[] args) {
+        System.out.println("xxxxxx");
+
+    }
+
+//    public static String xys(String changeX) {
+//        if (changeX.charAt(changeX.length()) == "x") {
+//            return changeX.charAt() = "y";
+//        }
+//        return
+//    }
+}
+
 // Given a string, compute recursively (no loops) a new string where all the
 // lowercase 'x' chars have been changed to 'y' chars.
