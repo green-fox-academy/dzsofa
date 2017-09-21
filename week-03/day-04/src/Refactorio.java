e4a09fba6332dcb1073f849fef15d80b5d100798
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-21 12:54:48 +0200

    week03/day04 Refactorio done

--- week-03/day-04/src/Refactorio.java
+++ week-03/day-04/src/Refactorio.java
@@ -1,2 +1,16 @@
+public class Refactorio {
+    public static void main(String[] args) {
+        
+        System.out.println(refactorio(5));
+
+    }
+
+    public static int refactorio(int number) {
+        if (number == 0 || number == 1) {
+            return 1;
+        }
+        return refactorio(number - 1) * number;
+    }
+}
 // - Create a recursive function called `refactorio`
 //   that returns it's input's factorial
