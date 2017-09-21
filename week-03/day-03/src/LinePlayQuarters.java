534765d8a8953c0549e52558cb5a684f8a639d22
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-21 09:31:35 +0200

    renamed week-03/day-01 dir to week-03/day-02

--- week-03/day-03/src/LinePlayQuarters.java
+++ week-03/day-03/src/LinePlayQuarters.java
@@ -6,12 +6,12 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class LinePlayQuarters {
 
-    public static void mainDraw(Graphics graphics){
-      // divide the canvas into 4 parts
-      // and repeat this pattern in each quarter:
-      // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
+    public static void mainDraw(Graphics graphics) {
+        // divide the canvas into 4 parts
+        // and repeat this pattern in each quarter:
+        // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
 
-
+        //top left
         int x1 = 10;
         int y2 = 10;
         for (int i = 0; i < 16; i++) {
@@ -27,9 +27,9 @@ public class LinePlayQuarters {
             graphics.drawLine(2, y1, x2, 169);
             x2 += 10;
             y1 += 10;
-
         }
 
+        //top right
         int x3 = 168;
         int y4 = 10;
         for (int i = 0; i < 16; i++) {
@@ -48,10 +48,33 @@ public class LinePlayQuarters {
 
         }
 
-
+        //bottom left
+//        int x5 = 10;
+//        int y6 = 338;
+//        for (int i = 0; i < 16; i++) {
+//            graphics.setColor(new Color(226, 0, 116));
+//            graphics.drawLine(x5, 179, 158, y6);
+//            x5 += 10;
+//            y6 += 10;
+//
+//
+//        }
     }
 
-//    Don't touch the code below
+//    public static void quarterLines(Graphics g, int[] coordinates) {
+//        for (int i = 0; i < 16; i++) {
+//            g.setColor(new Color(226, 0, 116));
+//            g.drawLine(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+//            coordinates[0] += 10;
+//            coordinates[3] += 10;
+//        }
+//        for (int i = 0; i < 16; i++) {
+//            g.setColor(Color.GREEN);
+//            g.drawLine(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+//        }
+//    }
+
+    //    Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 343;
 
@@ -63,7 +86,8 @@ public class LinePlayQuarters {
         jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
     }
-    static class ImagePanel extends JPanel{
+
+    static class ImagePanel extends JPanel {
         @Override
         protected void paintComponent(Graphics graphics) {
             super.paintComponent(graphics);
