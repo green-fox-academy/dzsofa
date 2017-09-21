41dae79f61f31166513dddf41ae2f9de89400a7c
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-21 11:53:19 +0200

    week03/day04 Bunny1 task done, compputed all the bunnEars :)

--- week-03/day-04/src/Bunny1.java
+++ week-03/day-04/src/Bunny1.java
@@ -1,2 +1,16 @@
-// We have a number of bunnies and each bunny has two big floppy ears.
-// We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+public class Bunny1 {
+    // We have a number of bunnies and each bunny has two big floppy ears.
+    // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+    public static void main(String[] args) {
+        System.out.println(bunnEars(5));
+    }
+
+    public static int bunnEars(int bunNum) {
+        if (bunNum == 1) {
+            return 2;
+        }
+        return EARS + bunnEars(bunNum - 1);
+    }
+
+    static int EARS = 2;
+}
