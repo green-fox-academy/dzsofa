08e63cf7411c41d0a1bd196ade5f48b6fe8883f6
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-21 16:00:09 +0200

    week03/day04 String3 started

--- week-03/day-04/src/String3.java
+++ week-03/day-04/src/String3.java
@@ -1,2 +1,14 @@
+public class String3 {
+    public static void main(String[] args) {
+        System.out.println(asterisk("xyesxxdsxfdj"));
+    }
+    public static String asterisk(String giveMeAsterisk) {
+        if (giveMeAsterisk.length() == 0) {
+            return "";
+        }
+        String pre = giveMeAsterisk.matches("^[A-I].*$") ? "*" : giveMeAsterisk.substring(0, 1);
+        return asterisk(giveMeAsterisk.substring(1)) + "*";
+    }
+}
 // Given a string, compute recursively a new string where all the
 // adjacent chars are now separated by a "*".
