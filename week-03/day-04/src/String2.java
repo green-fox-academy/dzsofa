9cf9d2cea9778c4b1a5c0954532f4d6d0c29ac98
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-21 15:47:40 +0200

    week03/day04 String2 done

--- week-03/day-04/src/String2.java
+++ week-03/day-04/src/String2.java
@@ -1,1 +1,14 @@
+public class String2 {
+    public static void main(String[] args) {
+        System.out.println(removeX("xyesxxdsxfdj"));
+    }
+
+    public static String removeX(String withX) {
+        if (withX.length() == 0) {
+            return "";
+        }
+        String pre = withX.startsWith("x") ? "" : withX.substring(0, 1);
+        return pre + removeX(withX.substring(1));
+    }
+}
 // Given a string, compute recursively a new string where all the 'x' chars have been removed.
