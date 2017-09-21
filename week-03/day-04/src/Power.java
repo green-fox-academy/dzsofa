a5565838e83ca4cae31df963af84ddb9f46583cd
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-21 11:38:43 +0200

    week03/day04 Power done

--- week-03/day-04/src/Power.java
+++ week-03/day-04/src/Power.java
@@ -1,2 +1,15 @@
+public class Power {
+    public static void main(String[] args) {
+
 // Given base and n that are both 1 or more, compute recursively (no loops)
 // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+        System.out.println(powerN(4, 1));
+
+    }
+    public static int powerN(int base, int power) {
+        if (base == 1 || power == 1) {
+            return base;
+        }
+        return base * powerN(base, power - 1);
+    }
+}
--- week-03/day-04/src/SumDigit.java
+++ week-03/day-04/src/SumDigit.java
@@ -3,11 +3,11 @@ public class SumDigit {
     // Given a non-negative int n, return the sum of its digits recursively (no loops).
     // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while
     // divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
-        System.out.println(sumDigit(3362));
+        System.out.println(sumDigit(5498));
 
     }
     public static int sumDigit(int inputNumber) {
-        if (inputNumber % 10 == inputNumber) {
+        if (inputNumber % 10 == inputNumber || inputNumber < 0) {
             return inputNumber;
         }
         return (inputNumber % 10) + sumDigit(inputNumber / 10);
