263587bdf09f66d219463060a8e5b5918398e364
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-21 12:01:44 +0200

    week03/day04 computed mutant bunnEars

--- week-03/day-04/src/Bunny1.java
+++ week-03/day-04/src/Bunny1.java
@@ -2,7 +2,7 @@ public class Bunny1 {
     // We have a number of bunnies and each bunny has two big floppy ears.
     // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
     public static void main(String[] args) {
-        System.out.println(bunnEars(5));
+        System.out.println(bunnEars(10));
     }
 
     public static int bunnEars(int bunNum) {
--- week-03/day-04/src/Bunny2.java
+++ week-03/day-04/src/Bunny2.java
@@ -1,3 +1,18 @@
+public class Bunny2 {
+    public static void main(String[] args) {
+        System.out.println(bunnEars(10));
+
+    }
+
+    public static int bunnEars(int bunNum) {
+        if (bunNum == 1) {
+            return 3;
+        } else if (bunNum % 2 == 1) {
+            return 3 + bunnEars(bunNum - 1);
+        }
+        return 2 + bunnEars(bunNum - 1);
+    }
+}
 // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
 // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
 // have 3 ears, because they each have a raised foot. Recursively return the
