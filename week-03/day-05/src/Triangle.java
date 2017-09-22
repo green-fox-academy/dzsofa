d1a2aca6aa1da376b1a81099c16147464c7f2112
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-22 10:55:00 +0200

    week03/day05 Circleception reformatted

--- week-03/day-05/src/Triangle.java
+++ week-03/day-05/src/Triangle.java
@@ -5,28 +5,22 @@ import java.util.HashMap;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class Circle {
+public class Triangle {
 
     public static void mainDraw(Graphics graphics) {
-//        graphics.drawOval(110, 110, 10, 10);
-        circleDraw(graphics, WIDTH / 8, HEIGHT / 8, 450);
+
 
     }
 
-    public static void circleDraw(Graphics graphics, int x, int y, int size) {
+    public static void triangleDraw(Graphics graphics, int x, int y, int size) {
         if (size < 10) {
             return;
         }
-        int r = 255 - (int) (Math.random() * 255);
-        int g = 255 - (int) (Math.random() * 255);
-        int b = 255 - (int) (Math.random() * 255);
-        graphics.drawOval(x, y, size, size);
-        graphics.setColor(new Color(r, g, b));
-        circleDraw(graphics, x + size / 4, y, size / 2);
-        graphics.setColor(new Color(r, g, b));
-        circleDraw(graphics, x + size / 24, (int) (y + size / 2.57), size / 2);
-        graphics.setColor(new Color(r, g, b));
-        circleDraw(graphics, (int) (x + size / 2.18), (int) (y + size / 2.57), size / 2);
+
+//        int r = 255 - (int) (Math.random() * 255);
+//        int g = 255 - (int) (Math.random() * 255);
+//        int b = 255 - (int) (Math.random() * 255);
+
     }
 
 
