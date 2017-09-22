972aa48bd2543338fbdbc9e52c69944e6b6582ca
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-22 10:21:26 +0200

    week03/day05 Circleception done

--- week-03/day-05/src/Circle.java
+++ week-03/day-05/src/Circle.java
@@ -8,20 +8,19 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class Circle {
 
     public static void mainDraw(Graphics graphics) {
-        graphics.drawOval(110, 110, 10, 10);
-        circleDraw(graphics, 110, 110, 360);
-
+//        graphics.drawOval(110, 110, 10, 10);
+        circleDraw(graphics, WIDTH / 8, HEIGHT / 8, 450);
 
     }
 
     public static void circleDraw(Graphics graphics, int x, int y, int size) {
-        if (size < 100) {
+        if (size < 10) {
             return;
         }
         graphics.drawOval(x, y, size, size);
-        circleDraw(graphics, x + 90, y, size / 2);
-        circleDraw(graphics, x + 15, y + 140, size / 2);
-        circleDraw(graphics, x + 165, y + 140, size / 2);
+        circleDraw(graphics, x + size / 4, y, size / 2);
+        circleDraw(graphics, x + size / 24, (int)(y + size / 2.57), size / 2);
+        circleDraw(graphics, (int)(x + size / 2.18), (int)(y + size / 2.57), size / 2);
     }
 
 
