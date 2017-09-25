e0d9b42d71e3d1269ba39025204c95801f306ade
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-25 12:00:51 +0200

    week04-day01 BlogPost done

--- week-04/day-01/src/BlogPost.java
+++ week-04/day-01/src/BlogPost.java
@@ -11,3 +11,48 @@
 //    - A popular long-form, stick-figure-illustrated blog about almost everything.
 //  - "One Engineer Is Trying to Get IBM to Reckon With Trump" titled by William Turton at "2017.03.28."
 //    - Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.
+
+public class BlogPost {
+    String authorName;
+    String title;
+    String text;
+    String publicationDate;
+
+    public BlogPost() {
+
+    }
+
+    public BlogPost(String authorName, String title, String text, String publicationDate) {
+        this.authorName = authorName;
+        this.title = title;
+        this.text = text;
+        this.publicationDate = publicationDate;
+
+    }
+
+}
+
+class ExBlogPost {
+    public static void main(String[] args) {
+        BlogPost bP1 = new BlogPost();
+        bP1.authorName = "John Doe";
+        bP1.title = "Lorem Ipsum";
+        bP1.text = "Lorem ipsum dolor sit amet.";
+        bP1.publicationDate = "2000.05.04.";
+        System.out.println(bP1.authorName + ", " + bP1.title);
+
+        BlogPost bP2 = new BlogPost();
+        bP2.authorName = "Tim Urban";
+        bP2.title = "Wait but why";
+        bP2.text = "A popular long-form, stick-figure-illustrated blog about almost everything.";
+        bP2.publicationDate = "2010.10.10.";
+        System.out.println(bP2.publicationDate + ", " + bP2.text);
+
+        BlogPost bP3 = new BlogPost();
+        bP3.authorName = "William Turton";
+        bP3.title = "One Engineer Is Trying to Get IBM to Reckon With Trump";
+        bP3.text = "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.";
+        bP3.publicationDate = "2017.03.28.";
+
+    }
+}
