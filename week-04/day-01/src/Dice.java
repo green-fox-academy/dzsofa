155a9f15eec395d2bae4d1454edce190b64b03e7
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-25 14:31:20 +0200

    week04-day01 Domino done, Dice started

---
+++ week-04/day-01/src/Dice.java
@@ -0,0 +1,63 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Dice {
+    //    You have a Dice class which has 6 dice
+//    You can roll all of them with roll()
+//    Check the current rolled numbers with getCurrent()
+//    You can reroll with reroll()
+//    Your task is to get where all dice is a 6
+    int[] dices = new int[6];
+
+    public int[] roll() {
+        for (int i = 0; i < dices.length; i++) {
+            dices[i] = (int) (Math.random() * 6) + 1;
+        }
+        return dices;
+    }
+
+    public int[] getCurrent() {
+        return dices;
+    }
+
+    public int getCurrent(int i) {
+        return dices[i];
+    }
+
+    public void reroll() {
+        for (int i = 0; i < dices.length; i++) {
+            dices[i] = (int) (Math.random() * 6) + 1;
+        }
+    }
+
+    public void reroll(int k) {
+        dices[k] = (int) (Math.random() * 6) + 1;
+    }
+
+    public static void main(String[] args) {
+        Dice myDice = new Dice();
+        myDice.getCurrent();
+        myDice.roll();
+        myDice.getCurrent();
+        myDice.getCurrent(5);
+        myDice.reroll();
+        myDice.getCurrent();
+        myDice.reroll(4);
+        myDice.getCurrent();
+
+        ArrayList<Integer> dice = new ArrayList<>();
+
+        for (int i = 0; i < myDice.dices.length; i++) {
+            if (myDice.dices[i] == 6) {
+                dice.add(myDice.dices[i]);
+                myDice.
+            } else {
+                myDice.reroll();
+            }
+            System.out.println(dice);
+        }
+    }
+
+
+}
---
+++ week-04/day-01/src/Domino.java
@@ -0,0 +1,60 @@
+
+import com.sun.java.browser.plugin2.DOM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Arrays;
+
+public class Domino {
+    private final int[] values;
+
+    public Domino(int valueA, int valueB) {
+        this.values = new int[]{valueA, valueB};
+    }
+
+    public int[] getValues() {
+        return values;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + values[0] + ", " + values[1] + "]";
+    }
+
+
+}
+
+class Dominoes {
+    public static void main(String[] args) {
+        List<Domino> dominoes = initializeDominoes();
+        // You have the list of Dominoes
+        // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+        // eg: [2, 4], [4, 3], [3, 5] ...
+
+        List<Domino> dominoesNew = new ArrayList<>();
+        dominoesNew.add(dominoes.get(0));
+
+        for (int i = 0; i < dominoes.size(); i++) {
+            for (int j = 1; j < dominoes.size(); j++) {
+                int[] domin1 = dominoesNew.get(i).getValues();
+                int[] domin2 = dominoes.get(j).getValues();
+                if (domin1[1] == domin2[0]) {
+                    dominoesNew.add(dominoes.get(j));
+                }
+            }
+        }
+        System.out.println(dominoesNew);
+    }
+
+    static List<Domino> initializeDominoes() {
+        List<Domino> dominoes = new ArrayList<>();
+        dominoes.add(new Domino(5, 2));
+        dominoes.add(new Domino(4, 6));
+        dominoes.add(new Domino(1, 5));
+        dominoes.add(new Domino(6, 7));
+        dominoes.add(new Domino(2, 4));
+        dominoes.add(new Domino(7, 1));
+        return dominoes;
+    }
+}
