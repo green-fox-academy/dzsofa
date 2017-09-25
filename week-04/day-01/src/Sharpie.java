a4de6ef024297ed74a3385c58f970f5f2aa4d424
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-25 17:09:28 +0200

    week04-day01 Sharpie

---
+++ week-04/day-01/src/Sharpie.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Sharpie {
+    String color;
+    float width;
+    float inkAmount = 100;
+
+    public Sharpie(String color, float width) {
+        this.color = color;
+        this.width = width;
+    }
+
+//    public Sharpie() {
+//        this.inkAmount = 100;
+//    }
+
+    public float use() {
+        return inkAmount - (float)0.5;
+    }
+
+    public static void main(String[] args) {
+        Sharpie green = new Sharpie("green", 1.5f);
+        System.out.println(green.use());
+    }
+}
