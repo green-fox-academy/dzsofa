41f62349be0f33e1c2e68c8acce4c656e21c8c3b
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-26 14:30:57 +0200

    w04-d02 GreenFoxInheritance, PallidaClass created

---
+++ week-04/day-02/src/PallidaClass.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class PallidaClass {
+    String classname;
+    ArrayList<Student> students;
+    ArrayList<Mentor> mentors;
+
+    public PallidaClass(String classname) {
+        this.classname = classname;
+        ArrayList<Student> students = new ArrayList<>();
+        ArrayList<Mentor> mentors = new ArrayList<>();
+    }
+
+    public void addStudent(Student st) {
+        students.add(st);
+    }
+
+    public void addMentor(Mentor m) {
+        mentors.add(m);
+    }
+
+    public void info() {
+        System.out.println("Pallida " + classname + " class has " + students.size() + " students and " + mentors.size() + " mentors.");
+    }
+}
