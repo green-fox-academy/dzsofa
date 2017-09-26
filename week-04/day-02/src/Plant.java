7085023d75d6e06e0f9fd8fb514845aff41314af
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-26 15:32:54 +0200

    w04-d02 Garden task, Plant class added

---
+++ week-04/day-02/src/Plant.java
@@ -0,0 +1,21 @@
+public class Plant {
+    String name;
+    int currentWaterAmount;
+    int waterAmount;
+
+    public Plant(String color) {
+        this.name = name;
+    }
+
+    public Plant() {
+
+    }
+
+    public void needsWater() {
+    }
+
+    public void watering() {
+    }
+
+
+}
