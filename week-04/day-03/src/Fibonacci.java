36d53dfc438f6b7c6214d29c83d156fe851b31f1
Zsofia <zsofi.dedeo@mensa.hu>
2017-09-27 14:37:32 +0200

    w04-d03 Fibonacci method done

---
+++ week-04/day-03/src/Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(7));
+
+    }
+
+    public static int fibonacci(int number) {
+        if (number == 0) {
+            return 0;
+        }
+        if (number == 1) {
+            return 1;
+        } else {
+            number = fibonacci(number - 1) + fibonacci(number - 2);
+        }
+        return number;
+    }
+}
