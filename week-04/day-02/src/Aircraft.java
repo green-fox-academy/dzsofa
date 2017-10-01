3c8a64d6042398731953ad6a03f043b2a49a0915
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-01 14:59:04 +0200

    some progress with Aircraft class and Carrier, fill method done

--- week-04/day-02/src/Aircraft.java
+++ week-04/day-02/src/Aircraft.java
@@ -1,9 +1,8 @@
 public class Aircraft {
     int maxAmmo;
     protected int initAmmo;
-    protected int ammoStore;
+    protected int acAmmoStore;
     protected int baseDamage;
-    int allDamage;
     String type;
 
     public Aircraft(String type, int maxAmmo, int baseDamage) {
@@ -16,23 +15,18 @@ public class Aircraft {
         this.initAmmo = 0;
     }
 
-    public String fight(Aircraft another) {
-        another.allDamage = another.baseDamage * this.ammoStore;
-        this.ammoStore = 0;
-        return "The damage suffered by " + another.getType() + " is " + another.allDamage + ".";
+    public int fight() {
+        return acAmmoStore * baseDamage;
     }
 
-    public String refill(int amount) {
-        if (amount < maxAmmo - ammoStore) {
-            ammoStore += amount;
-            return "The total amount of ammo in your storage is: " + ammoStore;
-        } else if (amount == maxAmmo - ammoStore) {
-            ammoStore += amount;
-            return "Your storage is full, nothing remains in filling station.";
-        }
-        else {
-            ammoStore = maxAmmo;
-            return "Your storage is full, " + (amount - maxAmmo) + " in filling station.";
+    public int refill(int amount) {
+        if (amount <= maxAmmo - acAmmoStore) {
+            acAmmoStore += amount;
+            return 0;
+        } else {
+            int fill = maxAmmo - acAmmoStore;
+            acAmmoStore = maxAmmo;
+            return amount - fill;
         }
     }
 
@@ -42,6 +36,6 @@ public class Aircraft {
 
 
     public String getStatus() {
-        return "Type: " + type + ", Ammo: " + ammoStore + ", Base Damage: " + baseDamage + ", All Damage: " + allDamage;
+        return "Type: " + type + ", Ammo: " + acAmmoStore + ", Base Damage: " + baseDamage + ", All Damage: " + fight() + "\n";
     }
 }
--- week-04/day-02/src/Carrier.java
+++ week-04/day-02/src/Carrier.java
@@ -6,37 +6,60 @@ public class Carrier {
     int healthPoints;
 
     public Carrier(int ammoStore, int healthPoints) {
+        this.aircrafts = new ArrayList<>();
         this.ammoStore = ammoStore;
         this.healthPoints = healthPoints;
     }
 
-    public Carrier() {
-        this.aircrafts = new ArrayList<>();
-    }
-
     public void addAircraft(Aircraft ac) {
-        aircrafts.add(ac);
+        if (ac.getType().equals("F16")) {
+            aircrafts.add(ac);
+        } else {
+            aircrafts.add(0, ac);
+        }
     }
-
 
     public String fight(Carrier anotherCarrier) {
         for (int i = 0; i < aircrafts.size(); i++) {
-            anotherCarrier.healthPoints -= (this.aircrafts.get(i).ammoStore * anotherCarrier.aircrafts.get(i).baseDamage);
-            aircrafts.get(i).ammoStore = 0;
+            anotherCarrier.healthPoints -= (this.aircrafts.get(i).acAmmoStore * anotherCarrier.aircrafts.get(i).baseDamage);
+            aircrafts.get(i).acAmmoStore = 0;
         }
         return "The remaining hp is " + healthPoints + ".";
     }
 
+    public String getStatus() {
+        String stat = "HP: " + healthPoints + ", Aircraft count: " + aircrafts.size() + ", Ammo Storage: " + ammoStore + ", Total damage: " + getTotalDamage() + "\n";
+        for (int i = 0; i < aircrafts.size(); i++) {
+           stat += aircrafts.get(i).getStatus();
+        }
+        return stat;
+    }
+
+    private int getTotalDamage() {
+        int totalDamage = 0;
+        for (int i = 0; i < aircrafts.size(); i++) {
+            totalDamage += aircrafts.get(i).fight();
+        }
+        return totalDamage;
+    }
+
+    public void fill() {
+        for (int i = 0; i < aircrafts.size(); i++) {
+            ammoStore = aircrafts.get(i).refill(ammoStore);
+        }
+    }
 
     public static void main(String[] args) {
-        Carrier one = new Carrier();
+        Carrier one = new Carrier(20, 5000);
+        one.addAircraft(new F35());
         one.addAircraft(new F16());
+        one.addAircraft(new F35());
         one.addAircraft(new F16());
-        one.addAircraft(new F16());
-        one.addAircraft(new F16());
+        one.fill();
+        System.out.println(one.getStatus());
 
 
-        Carrier two = new Carrier();
+        Carrier two = new Carrier(200, 2500);
         two.addAircraft(new F16());
         two.addAircraft(new F16());
         two.addAircraft(new F16());
