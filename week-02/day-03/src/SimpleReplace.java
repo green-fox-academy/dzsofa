f84e7ba7489cfa12c5a1eaeb62bd6ba5fb9025fa
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-02 21:45:39 +0200

    w02-d03 SimpleReplace refactored

--- week-02/day-03/src/SimpleReplace.java
+++ week-02/day-03/src/SimpleReplace.java
@@ -1,14 +1,13 @@
 public class SimpleReplace {
     public static void main(String... args){
         String example = "In a dishwasher far far away";
-
-        String overridden = example.replace("dishwasher", "galaxy"); // make a new string with the replaced word
-
         // I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
         // Please fix it for me!
         // Expected ouput: In a galaxy far far away
 
-        System.out.println(overridden); // print out the new string
+        example = example.replace("dishwasher", "galaxy");
+
+        System.out.println(example);
     }
 }
 
