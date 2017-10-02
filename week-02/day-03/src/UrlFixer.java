bd96cedc58431a4336934bcf6e3a969917bdcd19
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-02 21:34:32 +0200

    w02-d03 UrlFixer optimized

--- week-02/day-03/src/UrlFixer.java
+++ week-02/day-03/src/UrlFixer.java
@@ -5,12 +5,10 @@ public class UrlFixer {
         // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
         // Also, the URL is missing a crutial component, find out what it is and insert it too!
         // Try to solve it more than once using different String functions!
-        String newURL = url.substring(0, 5) + ":" + url.substring(5);
-        newURL =  newURL.replaceAll("bots", "odds");
 
-        System.out.println(newURL);
-
-//        String newerURL = ;
+        url = url.substring(0, 5) + ":" + url.substring(5);
+        url = url.replaceAll("bots", "odds");
+        System.out.println(url);
     }
 }
 
