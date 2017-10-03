5a67e4de539cbd4a781d25d17ce22cc0e7c2ab6c
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-03 14:55:16 +0200

    ScrumGame refactored, wall positioning method called, initial hero moves working

---
+++ week-04/day-01/src/SharpieSetUse.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class SharpieSetUse {
+    public static void main(String[] args) {
+        ArrayList<Sharpie> sharpies = new ArrayList<>();
+        sharpies.add(new Sharpie("green", 0.5f));
+        sharpies.add(new Sharpie("yellow", 0.25f));
+        sharpies.add(new Sharpie("purple", 0.4f));
+        sharpies.add(new Sharpie("red", 0.4f));
+
+
+    }
+}
