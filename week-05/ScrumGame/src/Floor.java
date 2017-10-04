1f3957ce33a8d9bc44db987319a6e1671452291b
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-04 16:05:05 +0200

    ScrumGame, removed unused fields from Floor and Wall class

--- week-05/ScrumGame/src/Floor.java
+++ week-05/ScrumGame/src/Floor.java
@@ -1,9 +1,7 @@
 public class Floor extends PositionedImage {
-    boolean movable = true;
 
     public Floor(int posX, int posY) {
         super("src/assets/floor.png", posX, posY);
-        this.movable = true;
 
     }
 
--- week-05/ScrumGame/src/Wall.java
+++ week-05/ScrumGame/src/Wall.java
@@ -3,12 +3,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Wall extends PositionedImage {
-    boolean movable = false;
 
 
     public Wall(int posX, int posY) {
         super("src/assets/wall.png", posX, posY);
-        this.movable = false;
     }
 
 }
