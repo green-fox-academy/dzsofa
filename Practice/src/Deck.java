c6b9a371ac7b4ea567a3513d9ea543549bfbc200
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-14 11:59:16 +0200

    BlackJack, value of cards in Deck are now random

--- Practice/src/Deck.java
+++ Practice/src/Deck.java
@@ -1,18 +1,20 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Random;
 
 public class Deck {
     ArrayList<Card> deck;
     String[] color = {"Spades", "Hearts", "Diamonds", "Clubs"};
     String[] value = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    private Random rand = new Random();
 
     public Deck(int amount) {
         deck = new ArrayList<>();
         int count = 0;
         for (int i = 0; i < value.length && count < amount; i++) {
             for (int j = 0; j < color.length; j++) {
-                deck.add(new Card(color[j], value[i]));
+                deck.add(new Card(color[j], value[rand.nextInt(value.length)]));
                 count++;
             }
         }
