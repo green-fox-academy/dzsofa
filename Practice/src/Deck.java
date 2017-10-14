783820da9c2090f2632c5b3c994b149bd69f500d
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-14 11:33:10 +0200

    BlackJack, Deck class created, added Constructor and toString method for counting occurrences of colors

---
+++ Practice/src/Deck.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Deck {
+    ArrayList<Card> deck;
+    String[] color = {"Spades", "Hearts", "Diamonds", "Clubs"};
+    String[] value = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+
+    public Deck(int amount) {
+        deck = new ArrayList<>();
+        int count = 0;
+        for (int i = 0; i < value.length && count < amount; i++) {
+            for (int j = 0; j < color.length; j++) {
+                deck.add(new Card(color[j], value[i]));
+                count++;
+            }
+        }
+        Collections.shuffle(deck);
+    }
+
+    public void draw() {
+
+    }
+
+    public int countColor( String color) {
+        int count = 0;
+        for (int i = 0; i < deck.size(); i++) {
+            if (deck.get(i).color.equals(color)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return deck.size() + " Cards - " + countColor("Spades") + " Spades, " + countColor("Hearts") + " Hearts, " + countColor("Diamonds") + " Diamonds, " + countColor("Clubs") + " Clubs.";
+    }
+}
