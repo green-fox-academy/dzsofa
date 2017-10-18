6ef980272b79592f80337b4465ce103e8d00862b
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-18 17:01:52 +0200

    w06-d03, refactored Lottery class

--- week-06/day-03/lotteryfilter/src/main/java/Lottery.java
+++ week-06/day-03/lotteryfilter/src/main/java/Lottery.java
@@ -1,29 +1,25 @@
-import joptsimple.*;
-import com.opencsv.*;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
+import joptsimple.OptionSet;
+import joptsimple.OptionParser;
 
 public class Lottery {
 
     public static void main(String[] args) {
-        OptionParser parser = new OptionParser();
-        parser.accepts("y");
-        parser.accepts("f");
-        parser.accepts("o");
-        OptionSet options = parser.parse(args);
+
+        OptionParser parser = new OptionParser("yfo:");
+        String year = null;
+
+        OptionSet options = parser.parse("-y", year);
 
         if (options.has("y")) {
-            FileMethods.openFile("src/assets/otos.csv");
+            FileMethods.openFile("src/main/assets/otos.csv");
         } if (options.has("f")) {
 
         }
         if (options.has("o")) {
 
         }
+
+        FileMethods.openFile("src/main/assets/otos.csv");
     }
 
 
