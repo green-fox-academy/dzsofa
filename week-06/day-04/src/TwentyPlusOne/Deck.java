72b66754116db92211e5b77a1842fe398288a728
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-19 17:46:41 +0200

    TwentyPlusOne, Deck constructor created, shuffle() and toString() methods added

--- week-06/day-04/src/TwentyPlusOne/Deck.java
+++ week-06/day-04/src/TwentyPlusOne/Deck.java
@@ -1,10 +1,41 @@
-//package TwentyPlusOne;
-//
-//import java.util.ArrayList;
-//
-//public class Deck {
-//    ArrayList<Card> deck;
-//
-//    public Deck
-//
-//}
+package TwentyPlusOne;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+    ArrayList<Card> deck;
+
+    public Deck() {
+        deck = new ArrayList<>();
+        createDeck();
+        shuffle();
+    }
+
+    private void createDeck() {
+        for (int i = 0; i < Card.Color.values().length; i++) {
+            for (int j = 0; j < Card.Suit.values().length; j++) {
+                for (int k = 0; k < Card.Rank.values().length; k++) {
+                    deck.add(new Card(Card.Color.values()[i], Card.Suit.values()[j], Card.Rank.values()[k]));
+                }
+            }
+        }
+    }
+
+    private void shuffle() {
+        Collections.shuffle(this.deck);
+    }
+
+    public Card pullFirst() {
+        
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Card card : this.deck) {
+            sb.append(card.toString() + "\n");
+        }
+        return sb.toString();
+    }
+}
