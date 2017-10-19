12390ee8b31ddfad6c1bef30494811456b2fea3d
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-19 11:54:12 +0200

    CarType and Color enums created, added randomization

--- week-06/day-04/src/Car.java
+++ week-06/day-04/src/Car.java
@@ -1,2 +1,21 @@
+import java.util.Random;
+
 public class Car {
+    public enum CarType{OPEL, MAZDA, RENAULT, HONDA, PACKARD, CITROEN, HYUNDAI}
+    public enum Color{BLUE, SILVER, GRAY, RED, YELLOW}
+    Random random = new Random();
+
+
+    public Car() {
+        CarType type = getRandomType();
+        Color color = getRandomColor();
+    }
+
+    public static CarType getRandomType() {
+        return CarType.values()[(int) (Math.random() * CarType.values().length)];
+    }
+
+    public static Color getRandomColor() {
+        return Color.values()[(int) (Math.random() * Color.values().length)];
+    }
 }
