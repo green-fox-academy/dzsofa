214ff239d5c74159afcb5568c3a2c23ac08b9257
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-19 12:43:25 +0200

    ParkingLot class refactored, added countColor(), countType() methods

--- week-06/day-04/src/ParkingLot.java
+++ week-06/day-04/src/ParkingLot.java
@@ -1,27 +1,64 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ParkingLot {
-    public static void main(String[] args) {
+    ArrayList<Car> parkingLot;
 
-        Car car = new Car();
-        System.out.println(car);
-
-        ArrayList<Car> parkingLot = new ArrayList<>();
-        ParkingLot.addCars(parkingLot);
-        getCars(parkingLot);
-
-
+    public ParkingLot() {
+        parkingLot = new ArrayList<>();
     }
 
-    public static void addCars(ArrayList<Car> cars) {
+    private void add() {
+        this.parkingLot.add(new Car());
+    }
+
+    public void addCars() {
         for (int i = 0; i < 256; i++) {
-            cars.add(new Car());
+            this.parkingLot.add(new Car());
         }
     }
 
-    public static void getCars(ArrayList<Car> cars) {
-        for (Car car : cars) {
+    public void getCars() {
+        for (Car car : this.parkingLot) {
             System.out.println(car);
         }
     }
+
+    //counts occurrence of car types
+    public void countTypes() {
+        Map<String, Integer> typeCount = new HashMap<>();
+        for (int i = 0; i < this.parkingLot.size(); i++) {
+            String carType = this.parkingLot.get(i).getType();
+            if (typeCount.containsKey(carType)) {
+                int count = typeCount.get(carType);
+                typeCount.put(carType, ++count);
+            } else {
+                typeCount.put(carType, 1);
+            }
+        }
+        for (Map.Entry entry : typeCount.entrySet()) {
+            System.out.print(entry + " ");
+        }
+        System.out.println();
+    }
+
+    //counts occurrence of colors
+    public void countColors() {
+        Map<String, Integer> colorCount = new HashMap<>();
+        for (int i = 0; i < this.parkingLot.size(); i++) {
+            String carColor = this.parkingLot.get(i).getColor();
+            if (colorCount.containsKey(carColor)) {
+                int count = colorCount.get(carColor);
+                colorCount.put(carColor, ++count);
+            } else  {
+                colorCount.put(carColor, 1);
+            }
+        }
+        for (Map.Entry entry : colorCount.entrySet()) {
+            System.out.println(entry + " ");
+        }
+        System.out.println();
+    }
+
 }
