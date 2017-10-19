3b2dea96593f36efbb80d2759fb570aa713f41ec
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-19 12:43:48 +0200

    ParkingLot, added separate class to run main()

---
+++ week-06/day-04/src/ParkingMain.java
@@ -0,0 +1,14 @@
+public class ParkingMain {
+    public static void main(String[] args) {
+
+
+        ParkingLot parkingLot = new ParkingLot();
+
+        parkingLot.addCars();
+//        parkingLot.getCars();
+
+//        parkingLot.countTypes();
+
+        parkingLot.countColors();
+    }
+}
