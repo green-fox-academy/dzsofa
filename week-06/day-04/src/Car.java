70e61f29fcf25678676ec1825a347e9bbe6f60dd
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-19 12:42:38 +0200

    Car toString() method refactored, now it uses getType() and getColor() methods

--- week-06/day-04/src/Car.java
+++ week-06/day-04/src/Car.java
@@ -29,7 +29,7 @@ public class Car {
 
     @Override
     public String toString() {
-        return this.color.toString() + " " + this.type.toString();
+        return getColor() + " " + getType();
 
     }
 }
