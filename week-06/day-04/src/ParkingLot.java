69ca371a8b8e8d158f13d9528de35d89783a2be8
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-19 12:13:30 +0200

    w06-d04 ParkingLot class created with list elements

--- week-06/day-04/src/ParkingLot.java
+++ week-06/day-04/src/ParkingLot.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class ParkingLot {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        System.out.println(car);
+
+        ArrayList<Car> parkingLot = new ArrayList<>();
+        ParkingLot.addCars(parkingLot);
+        getCars(parkingLot);
+
+
+    }
+
+    public static void addCars(ArrayList<Car> cars) {
+        for (int i = 0; i < 256; i++) {
+            cars.add(new Car());
+        }
+    }
+
+    public static void getCars(ArrayList<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car);
+        }
+    }
 }
