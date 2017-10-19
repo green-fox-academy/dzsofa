396608eb142cede206c00f60315edb737a43de5c
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-19 20:25:24 +0200

    SplitString done

---
+++ week-06/day-04/src/SplitString.java
@@ -0,0 +1,25 @@
+public class SplitString {
+    public static void main(String[] args) {
+
+        String[] myArray = splitString("lovebarbara", 4);
+
+        for (String s : myArray) {
+            if (s == myArray[0]) {
+                System.out.print(s + ", ");
+            } else {
+                System.out.print(s);
+            }
+        }
+    }
+
+    private static String[] splitString(String input, int index) {
+        String[] output = new String[2];
+        try {
+            output[0] = input.substring(0, index);
+            output[1] = input.substring(index);
+        }catch (IndexOutOfBoundsException e) {
+            System.out.println("Out of bounds");
+        }
+        return output;
+    }
+}
