9f3ed3c40f2778c76d28d2127512ac22cb27536c
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-20 11:33:35 +0200

    TodoApp, TodoList has toString() and getTodo() methods. getTodo() handles IndexOutOfBounds

--- week-06/day-05/todoapp/src/main/java/TodoList.java
+++ week-06/day-05/todoapp/src/main/java/TodoList.java
@@ -11,8 +11,21 @@ public class TodoList {
         this.todos.add(new Todo(description));
     }
 
-    public Todo getTodo(int index) {
-        return this.todos.get(index - 1);
+    public Todo getTodo(int nr) {
+        if (nr - 1 > todos.size() - 1 || nr - 1 == 0) {
+            System.out.println("Todo with given id doesn't exist.\nLast entry:");
+            return todos.get(todos.size() - 1);
+        } else {
+            return this.todos.get(nr - 1);
+        }
     }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Todo todo : todos) {
+            sb.append(todo + "\n");
+        }
+        return sb.toString();
+    }
 }
