97a35055e1874da7cbdcd8339022744b834b2199
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-23 10:55:55 +0200

    some more refactoring with LotteryFilter, doesn't work yet

--- week-06/day-03/lotteryfilter/src/main/java/FileMethods.java
+++ week-06/day-03/lotteryfilter/src/main/java/FileMethods.java
@@ -1,23 +1,12 @@
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class FileMethods {
-
-    public static void openFile(String file) {
-        CSVReader reader;
-        try {
-            reader = new CSVReader(new FileReader(file));
-            List<String[]> lines = reader.readAll();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
 
 }
--- week-06/day-03/lotteryfilter/src/main/java/Lottery.java
+++ week-06/day-03/lotteryfilter/src/main/java/Lottery.java
@@ -1,25 +1,59 @@
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 import joptsimple.OptionSet;
 import joptsimple.OptionParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Lottery {
 
     public static void main(String[] args) {
+        argHandling(args);
 
-        OptionParser parser = new OptionParser("yfo:");
-        String year = null;
+    }
 
-        OptionSet options = parser.parse("-y", year);
+    public static void argHandling(String[] input) {
+        OptionParser parser = new OptionParser();
+        parser.accepts("y").withRequiredArg();
+        parser.accepts("f").withRequiredArg();
+        parser.accepts("o").withRequiredArg();
+
+        OptionSet options = parser.parse(input);
+        String year = String.valueOf(options.valueOf("y"));
+        String inputFile = String.valueOf(options.valueOf("f"));
+        String outputFile = String.valueOf(options.valueOf("o"));
 
         if (options.has("y")) {
-            FileMethods.openFile("src/main/assets/otos.csv");
-        } if (options.has("f")) {
+            try {
+                if (options.has("f")) {
+                    CSVReader reader = new CSVReader(new FileReader("src/main/java/" + inputFile));
+                }
+                CSVReader otosreader = new CSVReader(new FileReader("src/main/java/otos.csv"));
+                CSVWriter writer = new CSVWriter(new FileWriter("src/main/java/output.csv"));
+                List<String[]> otosLines = otosreader.readAll();
+                List<String[]> temp = new ArrayList<>();
 
+                for (int i = 0; i < otosLines.size(); i++) {
+                    if (otosLines.get(i)[0].startsWith(year)) {
+                        temp.add(otosLines.get(i));
+                    }
+                }
+                if (options.has("o")) {
+                    writer = new CSVWriter(new FileWriter("src/main/java/" + inputFile));
+                    writer.writeAll(temp);
+                    writer.close();
+                }
+                writer.writeAll(temp);
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("can't read the file, try another");
+            }
         }
-        if (options.has("o")) {
-
-        }
-
-        FileMethods.openFile("src/main/assets/otos.csv");
     }
 
 
