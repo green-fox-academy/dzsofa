f546a69ff275ccdbdc09d8caecefd1159f277b5f
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-24 13:58:47 +0200

    w07-d01, SpringStart, greet counter with AtomicLong done

--- week-07/springstart/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloRESTController.java
+++ week-07/springstart/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloRESTController.java
@@ -5,12 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
+    AtomicLong atomicLong = new AtomicLong();
 
     @RequestMapping(value = "/greeting")
     public Greeting greet(@RequestParam String name) {
-        Greeting greeting = new Greeting(1, "Hello, " + name);
+        Greeting greeting = new Greeting(atomicLong.incrementAndGet(), "Hello, " + name);
         return greeting;
     }
 }
