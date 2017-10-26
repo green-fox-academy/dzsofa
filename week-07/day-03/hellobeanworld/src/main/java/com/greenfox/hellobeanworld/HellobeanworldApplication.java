149697d5f623bbab408be9684299d3905d2e151d
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-26 11:00:19 +0200

    HelloBeanWorld initial commit

---
+++ week-07/day-03/hellobeanworld/src/main/java/com/greenfox/hellobeanworld/HellobeanworldApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.hellobeanworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobeanworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellobeanworldApplication.class, args);
+	}
+}
