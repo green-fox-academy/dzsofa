94041189208691f550901df28752aeded8abd906
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-26 11:14:35 +0200

    HelloBeanWorldApplication running with beans

--- week-07/day-03/hellobeanworld/src/main/java/com/greenfox/hellobeanworld/HellobeanworldApplication.java
+++ week-07/day-03/hellobeanworld/src/main/java/com/greenfox/hellobeanworld/HellobeanworldApplication.java
@@ -2,11 +2,20 @@ package com.greenfox.hellobeanworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class HellobeanworldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HellobeanworldApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        ApplicationContext appContext =
+                new AnnotationConfigApplicationContext(HelloWorldConfig.class);
+
+        HelloWorld helloWorld = appContext.getBean(HelloWorld.class);
+        helloWorld.setMessage("Hello World");
+        helloWorld.getMessage();
+
+    }
 }
