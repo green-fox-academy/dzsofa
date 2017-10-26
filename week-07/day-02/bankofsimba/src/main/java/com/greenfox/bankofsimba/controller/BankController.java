63630550a21e95e036233813c03626a5573b312f
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-26 16:21:21 +0200

    Bank of Simba, balance increases for all accounts

--- week-07/day-02/bankofsimba/src/main/java/com/greenfox/bankofsimba/controller/BankController.java
+++ week-07/day-02/bankofsimba/src/main/java/com/greenfox/bankofsimba/controller/BankController.java
@@ -9,35 +9,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class BankController {
-    ArrayList<BankAccount> bank;
+    List<BankAccount> bank = Arrays.asList(new BankAccount("Scar", 0, "lion", false, false),
+            (new BankAccount("Timon", 1500, "meerkat", false, true)),
+            (new BankAccount("Rafiki", 15000, "mandrill", false, true)),
+            (new BankAccount("Mufasa", 5000, "lion", false, true)),
+            (new BankAccount("Simba", 2000, "lion", true, true)));
 
-    public ArrayList<BankAccount> generateBank() {
-        bank = new ArrayList<>(Arrays.asList(new BankAccount("Scar", 0, "lion", false, false),
-                (new BankAccount("Timon", 1500, "meerkat", false, true)),
-                (new BankAccount("Rafiki", 15000, "mandrill", false, true)),
-                (new BankAccount("Mufasa", 5000, "lion", false, true)),
-                (new BankAccount("Simba", 2000, "lion", true, true))));
-        return bank;
+    public List<BankAccount> getBank() {
+        return this.bank;
     }
 
     @RequestMapping("/bank")
     public String bank(Model model) {
-        generateBank();
-        model.addAttribute("bank", bank);
+        model.addAttribute("bank", getBank());
         return "bankinfo";
     }
 
     @PostMapping("/increasebalance")
-    public String increase(@ModelAttribute BankAccount account) {
+    public String increase() {
         for (int i = 0; i < bank.size(); i++) {
-            bank.get(i).equals(account);
-            if (account.isKing) {
-                account.setBalance(account.getBalance() + 100);
+            if ( bank.get(i).isKing) {
+                bank.get(i).setBalance( bank.get(i).getBalance() + 100);
             } else {
-                account.setBalance(account.getBalance() + 10);
+                bank.get(i).setBalance( bank.get(i).getBalance() + 10);
             }
         }
         return "redirect:/bank";
