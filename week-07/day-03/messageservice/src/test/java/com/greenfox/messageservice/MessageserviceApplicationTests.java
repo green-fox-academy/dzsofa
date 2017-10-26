51c686e7e33f6df7403954c6060e865704f069a8
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-26 12:31:47 +0200

    MessageServices initial commit

---
+++ week-07/day-03/messageservice/src/test/java/com/greenfox/messageservice/MessageserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfox.messageservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MessageserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
