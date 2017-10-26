3530c3c0aafde1ba5e3488bafded819667f900c5
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-26 11:21:42 +0200

    PlayWithColors initial commit

---
+++ week-07/day-03/playcolors/src/main/java/com/greenfox/playcolors/PlaycolorsApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.playcolors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaycolorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaycolorsApplication.class, args);
+	}
+}
