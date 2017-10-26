a6d752ddca6e3848c4b2c75c9831c66fa11a3169
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-26 12:36:39 +0200

    PlayColors refactored

--- week-07/day-03/playcolors/src/main/java/com/greenfox/playcolors/PlaycolorsApplication.java
+++ week-07/day-03/playcolors/src/main/java/com/greenfox/playcolors/PlaycolorsApplication.java
@@ -1,8 +1,6 @@
 package com.greenfox.playcolors;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
--- week-07/day-03/playcolors/src/main/java/com/greenfox/playcolors/PurpleColor.java
+++ week-07/day-03/playcolors/src/main/java/com/greenfox/playcolors/PurpleColor.java
@@ -1,6 +1,5 @@
 package com.greenfox.playcolors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
