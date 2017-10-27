aa5367931eb71cf7638636c449652aab41fd7a17
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-27 12:21:47 +0200

    Fox class done, has configurator

---
+++ week-07/day-04/foxclub/src/main/java/com/greenfox/foxclub/configurator/FoxConfigurator.java
@@ -0,0 +1,16 @@
+package com.greenfox.foxclub.configurator;
+
+import com.greenfox.foxclub.model.Fox;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan
+public class FoxConfigurator {
+
+    @Bean
+    public Fox myFox() {
+        return new Fox();
+    }
+}
--- week-07/day-04/foxclub/src/main/java/com/greenfox/foxclub/model/Fox.java
+++ week-07/day-04/foxclub/src/main/java/com/greenfox/foxclub/model/Fox.java
@@ -1,15 +1,56 @@
 package com.greenfox.foxclub.model;
 
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+
 public class Fox {
     private String name;
-    private TrickList myTricks;
+    private ArrayList<Trick> myTricks;
     private Food myFood;
     private Drink myDrink;
+    private int knownTricks;
 
-    public Fox(String name, TrickList myTricks, Food myFood, Drink myDrink) {
+    public Fox() {
+        this.name = "Fixi-foxi";
+        this.myTricks = new ArrayList<>();
+        myFood = null;
+        myDrink = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
-        this.myTricks = myTricks;
+    }
+
+    public ArrayList<Trick> getMyTricks() {
+        return myTricks;
+    }
+
+    public int getKnownTricks() {
+        return myTricks.size();
+    }
+
+    public void addTrick(Trick trick) {
+        this.myTricks.add(trick);
+    }
+
+    public Food getMyFood() {
+        return myFood;
+    }
+
+    public void setMyFood(Food myFood) {
         this.myFood = myFood;
+    }
+
+    public Drink getMyDrink() {
+        return myDrink;
+    }
+
+    public void setMyDrink(Drink myDrink) {
         this.myDrink = myDrink;
     }
 }
