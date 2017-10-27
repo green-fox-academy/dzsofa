39118a0ec170b161e97cb577cf9b9ccacf84a247
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-27 12:23:08 +0200

    Fox Club, MainController has methods returning index and nutritioninfo

--- week-07/day-04/foxclub/src/main/java/com/greenfox/foxclub/controller/MainController.java
+++ week-07/day-04/foxclub/src/main/java/com/greenfox/foxclub/controller/MainController.java
@@ -1,13 +1,25 @@
 package com.greenfox.foxclub.controller;
 
+import com.greenfox.foxclub.model.Fox;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    Fox myFox;
+
     @RequestMapping({"", "/"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("fox", myFox);
         return "index";
+    }
+
+    @RequestMapping("/nutritionstore")
+    public String nutrition() {
+        return "nutritioninfo";
     }
 }
