eb6932529e29d95d4a81caa235b3b447832e58e9
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-28 12:54:32 +0200

    Fox Club, some refactoring

--- week-07/day-04/foxclub/src/main/java/com/greenfox/foxclub/controller/MainController.java
+++ week-07/day-04/foxclub/src/main/java/com/greenfox/foxclub/controller/MainController.java
@@ -1,10 +1,10 @@
 package com.greenfox.foxclub.controller;
 
-import com.greenfox.foxclub.model.Fox;
+import com.greenfox.foxclub.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
@@ -12,14 +12,24 @@ public class MainController {
     @Autowired
     Fox myFox;
 
-    @RequestMapping({"", "/"})
+    @GetMapping({"", "/"})
     public String index(Model model) {
         model.addAttribute("fox", myFox);
         return "index";
     }
 
-    @RequestMapping("/nutritionstore")
-    public String nutrition() {
+    @GetMapping("/nutritionstore")
+    public String nutrition(Model model) {
+        model.addAttribute("food", Food.class);
         return "nutritioninfo";
+    }
+//
+//    public String setNutrition()
+
+    @GetMapping("/trickcenter")
+    public String tricks(Model model) {
+        model.addAttribute("fox", myFox);
+        model.addAttribute("trick", Trick.class);
+        return "trickcenter";
     }
 }
