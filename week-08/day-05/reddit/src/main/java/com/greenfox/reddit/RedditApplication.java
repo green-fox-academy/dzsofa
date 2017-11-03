fa249d87e8b9b8b34748b8725e778b59e9473dba
Zsofia <zsofi.dedeo@mensa.hu>
2017-11-03 16:13:40 +0100

    Reddit WebApp, some more examples for creation of table

--- week-08/day-05/reddit/src/main/java/com/greenfox/reddit/RedditApplication.java
+++ week-08/day-05/reddit/src/main/java/com/greenfox/reddit/RedditApplication.java
@@ -22,5 +22,18 @@ public class RedditApplication implements CommandLineRunner {
 //        postRepository.save(new Post("funny cat picture"));
 //        postRepository.save(new Post("some yo momma joke"));
 //        postRepository.save(new Post("green fox review"));
+//        postRepository.save(new Post("lorem ipsum"));
+//        postRepository.save(new Post("fikusz kukisz"));
+//        postRepository.save(new Post("mr bean is the best"));
+//        postRepository.save(new Post("hello darkness my old friend"));
+//        postRepository.save(new Post("y u do dis"));
+//        postRepository.save(new Post("review my code review view"));
+//        postRepository.save(new Post("send me some cookie"));
+//        postRepository.save(new Post("i can give you a hug if you want"));
+//        postRepository.save(new Post("thank god it's friday"));
+//        postRepository.save(new Post("lemme out from here!!!"));
+//        postRepository.save(new Post("Keep the change you filthy animal!"));
+//        postRepository.save(new Post("Java 4eva!"));
+
     }
 }
