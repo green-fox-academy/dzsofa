eaa1baae3c2d10faaabb89be0b8f8687c4d00f3b
Zsofia <zsofi.dedeo@mensa.hu>
2017-11-06 12:04:34 +0100

    Tennis DOJO, TennisGame2 initial commit

--- week-09/day-01/tennisdojo/test/TennisTest.java
+++ week-09/day-01/tennisdojo/test/TennisTest.java
@@ -80,17 +80,17 @@ public class TennisTest {
         assertEquals(this.expectedScore, game.getScore());
     }
 
-    @Test
-    public void checkAllScoresTennisGame1() {
-        TennisGame1 game = new TennisGame1("player1", "player2");
-        checkAllScores(game);
-    }
-
 //    @Test
-//    public void checkAllScoresTennisGame2() {
-//        TennisGame2 game = new TennisGame2("player1", "player2");
+//    public void checkAllScoresTennisGame1() {
+//        TennisGame1 game = new TennisGame1("player1", "player2");
 //        checkAllScores(game);
 //    }
+
+    @Test
+    public void checkAllScoresTennisGame2() {
+        TennisGame2 game = new TennisGame2("player1", "player2");
+        checkAllScores(game);
+    }
 //
 //    @Test
 //    public void checkAllScoresTennisGame3() {
