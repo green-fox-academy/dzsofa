c0685498a971f10c90a0c8f92ce963525e34b7a2
Zsofia <zsofi.dedeo@mensa.hu>
2017-11-06 12:01:43 +0100

    Tennis DOJO, did some refactoring, different switch cases have different methods

--- week-09/day-01/tennisdojo/src/TennisGame.java
+++ week-09/day-01/tennisdojo/src/TennisGame.java
@@ -1,5 +1,6 @@
 public interface TennisGame {
 
     void wonPoint(String playerName);
+
     String getScore();
 }
--- week-09/day-01/tennisdojo/src/TennisGame1.java
+++ week-09/day-01/tennisdojo/src/TennisGame1.java
@@ -1,78 +1,99 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int player1Score = 0;
+    private int player2Score = 0;
+    private String score = "";
+    private int tempScore = 0;
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals("player1")) {
+            player1Score++;
+        } else {
+            player2Score++;
+        }
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        if (player1Score == player2Score) {
+            score = getEqualScores();
+        } else if (player1Score >= 4 || player2Score >= 4) {
+            score = getAdvantage();
+        } else {
+            score = getDifferentScores();
         }
         return score;
     }
+
+    private String getAdvantage() {
+        int scoreDifference = player1Score - player2Score;
+        if (scoreDifference == 1) {
+            score = "Advantage player1";
+        } else if (scoreDifference == -1) {
+            score = "Advantage player2";
+        } else if (scoreDifference >= 2) {
+            score = "Win for player1";
+        } else {
+            score = "Win for player2";
+        }
+        return score;
+    }
+
+
+    private String getDifferentScores() {
+
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) {
+                tempScore = player1Score;
+            } else {
+                score += "-";
+                tempScore = player2Score;
+            }
+            switch (tempScore) {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
+            }
+        }
+        return score;
+
+    }
+
+    public String getEqualScores() {
+        String score = "";
+        switch (player1Score) {
+            case 0:
+                score += "Love-All";
+                break;
+            case 1:
+                score += "Fifteen-All";
+                break;
+            case 2:
+                score += "Thirty-All";
+                break;
+            case 3:
+                score += "Forty-All";
+                break;
+            default:
+                score += "Deuce";
+                break;
+        }
+        return score;
+    }
+
+
 }
