29b65d1c1248a032b49259963d3b95eb85227fe6
Zsofia <zsofi.dedeo@mensa.hu>
2017-11-07 20:06:14 +0100

    Todo WebApp, fixed query in TodoRepo

--- week-08/day-02/connecttomysql/src/main/java/com/greenfox/connecttomysql/repository/TodoRepo.java
+++ week-08/day-02/connecttomysql/src/main/java/com/greenfox/connecttomysql/repository/TodoRepo.java
@@ -10,6 +10,6 @@ public interface TodoRepo extends CrudRepository<Todo, Long> {
 
     List<Todo> findAllByTitleContaining(String searchTerm);
 
-    @Query(value = "SELECT * FROM todos LEFT OUTER JOIN assignees ON todos.assignee_id = assignees.id WHERE title LIKE ?1 OR name LIKE ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM todos LEFT OUTER JOIN assignees ON todos.assignee_id = assignees.id WHERE title LIKE %?1% OR name LIKE %?1%", nativeQuery = true)
     List<Todo> findTitleAndAssignee(String searchTerm);
 }
